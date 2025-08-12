6cdea5ff2f0da2ff4af358d616ae9b27e45a59b1
MANOJ-62 <manoraj1108@gmail.com>
2025-08-12 18:11:11 +0530

    Maximum Product Subarray Leetcode 153

--- src/medium/FindMinimuminRotatedSortedArray.java
+++ src/medium/FindMinimuminRotatedSortedArray.java
@@ -4,7 +4,7 @@ package medium;
  * Leetcode 153
  * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
  * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
- * Find the minimum element.
+ * Find the minimum element
  * You must write an algorithm with O(log n) runtime complexity.
  */
 public class FindMinimuminRotatedSortedArray {
